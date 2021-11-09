9de11e1f97565546a72824401df609c96d86ccd4
chenhongguang <chenhongguang@m.scnu.edu.cn>
2021-11-09 17:47:10 +0800

    post.html add class select and tag select

--- tool/create_postlist.py
+++ tool/create_postlist.py
@@ -1,23 +1,42 @@
 import json
 import glob
-from json import decoder
 
-post_list_dic=dict()
-postlists=glob.glob("./post/*.md")
-id=1
+post_list_dic = {
+    "all": {
+
+    },
+    "classes": {
+        # classname : {}
+    },
+    "tags": {
+        # tagname : {
+        #   id : info}
+
+    }
+}
+postlists = glob.glob("./post/*.md")
+id = 1
 for post in postlists:
     name_, class_, tag_, time_ = post.split("_")
     url_ = name_
-    name_= name_.replace("./post/",'')
-    time_= time_.replace(".md","")
+    name_ = name_.replace("./post/", '')
+    time_ = time_.replace(".md", "")
     _tmp = dict()
     _tmp["name"] = name_
     _tmp["class"] = class_
     _tmp["tag"] = tag_
     _tmp["time"] = time_
     _tmp["url"] = url_
-    post_list_dic["%s"%id] = _tmp
-    id+=1
+    post_list_dic["all"]["%s" % id] = _tmp
+    if tag_ not in post_list_dic["tags"]:
+        post_list_dic["tags"][tag_] = dict()
+    post_list_dic["tags"][tag_]["%s" % id] = _tmp
 
-str_=json.dumps(post_list_dic,indent=4,ensure_ascii=False)
+    if class_ not in post_list_dic["classes"]:
+        post_list_dic["classes"][class_] = dict()
+    post_list_dic["classes"][class_]["%s" % id] = _tmp
+
+    id += 1
+
+str_ = json.dumps(post_list_dic, indent=4, ensure_ascii=False)
 print(str_)
